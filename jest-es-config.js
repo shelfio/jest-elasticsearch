92722e754a46078ad6f676e171a77a3b54d5efee
terret <andrew.petlya@gmail.com>
2020-01-22 17:58:36 +0200

    INFRA-794 feat: add arguments to generateMapping()

--- jest-es-config.js
+++ jest-es-config.js
@@ -1,23 +1,28 @@
 const documentsMapping = require('./documents-mapping');
 
-module.exports = function generateMapping() {
+module.exports = function generateMapping(
+  mappingSchema,
+  indexName,
+  aliasName,
+  elasticSearchVersion
+) {
   return {
-    esVersion: '6.8.2',
+    esVersion: elasticSearchVersion || '6.8.2',
     clusterName: 'docs',
     nodeName: 'docs',
     port: 9200,
     indexes: [
       {
-        name: 'documents',
+        name: indexName || 'documents',
         body: {
           settings: {
             number_of_shards: '1',
             number_of_replicas: '1'
           },
           aliases: {
-            'some-doc-id': {}
+            [aliasName || 'some-doc-id']: {}
           },
-          mappings: documentsMapping
+          mappings: mappingSchema || documentsMapping
         }
       }
     ]
