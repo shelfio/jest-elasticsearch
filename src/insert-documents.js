18c1548362911a6e0406340abc6e2d51d21504d4
terret <andrew.petlya@gmail.com>
2020-01-23 15:54:40 +0200

    feat: add the ability to accept multiple files (an array)

--- src/insert-documents.js
+++ src/insert-documents.js
@@ -1,18 +1,27 @@
 const {refreshAllIndexes, getClient} = require('./elasticsearch');
 
-module.exports = async function insertDocuments(document) {
-  const body = [
-    {update: {_index: 'documents', _id: document.id, routing: document.id}},
-    {
-      doc: document,
-      doc_as_upsert: true
-    }
-  ];
-
+module.exports = async function insertDocuments(documents) {
+  const body = generateTargetBody(documents);
   await bulk(body, {index: 'documents'});
-
   await refreshAllIndexes();
 };
+
+function generateTargetBody(documents) {
+  const targets = [];
+  documents.map(document => {
+    const body = [
+      {update: {_index: 'documents', _id: document.id, routing: document.id}},
+      {
+        doc: document,
+        doc_as_upsert: true
+      }
+    ];
+
+    return targets.push(...body);
+  });
+
+  return targets;
+}
 
 async function bulk(body, {index} = {}) {
   const client = getClient();
