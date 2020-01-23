f1fefa3d0dd9406e7eddf42f46092bf6e63d62ea
terret <andrew.petlya@gmail.com>
2020-01-23 15:57:54 +0200

    feat: add more documents in mock file

--- src/documents.mock.js
+++ src/documents.mock.js
@@ -1,4 +1,14 @@
-module.exports = {
-  id: 'some-doc-id',
-  name: 'some-name'
-};
+module.exports = [
+  {
+    id: 'some-doc-id-1',
+    name: 'some-name-1'
+  },
+  {
+    id: 'some-doc-id-2',
+    name: 'some-name-2'
+  },
+  {
+    id: 'some-doc-id-3',
+    name: 'some-name-3'
+  }
+];
