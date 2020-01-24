d95b12b9611441d0c4880cbe6e97c886f8c6172f
terret <andrew.petlya@gmail.com>
2020-01-24 18:41:46 +0200

    feat: update require config (as result of function)

--- setup.js
+++ setup.js
@@ -3,7 +3,7 @@ const cwd = require('cwd');
 const {start} = require('@shelf/elasticsearch-local');
 
 module.exports = async function startES() {
-  const config = require(resolve(cwd(), 'jest-es-config.js'));
+  const config = require(resolve(cwd(), 'jest-es-config.js'))();
 
   return start(config);
 };
