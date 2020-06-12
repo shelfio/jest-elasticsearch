2929c93f02292a97e9ddf8cf811e2b1beecc22ab
Vlad Holubiev <golubev.vld@gmail.com>
2020-06-12 13:50:25 +0300

    style: switch to @shelf/prettier-config

--- src/elasticsearch.test.js
+++ src/elasticsearch.test.js
@@ -1,5 +1,5 @@
 const {getClient} = require('./elasticsearch');
 
-it('should export getClient function', function() {
+it('should export getClient function', function () {
   expect(getClient).toBeInstanceOf(Function);
 });
