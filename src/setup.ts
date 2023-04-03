a3f73bc0aca5e4644e26a14e5d13503a10df6af7
Pavlo Batov <batovpasha@gmail.com>
2023-04-03 15:58:36 +0300

    feat: add support for custom config path through JEST_ELASTICSEARCH_CONFIG env

--- src/setup.ts
+++ src/setup.ts
@@ -3,8 +3,9 @@ import {start} from '@shelf/elasticsearch-local';
 
 const cwd = require('cwd');
 
-module.exports = async function startES() {
-  const config = require(resolve(cwd(), 'jest-es-config.js'))();
+module.exports = function startES() {
+  const path = process.env.JEST_ELASTICSEARCH_CONFIG || resolve(cwd(), 'jest-es-config.js');
+  const config = require(path)();
 
   return start(config);
 };
